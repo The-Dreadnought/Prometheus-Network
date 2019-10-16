5a15d0e6258c7b0a71b7ed6fc044aa233311e7e2
The-Dreadnought <56526236+The-Dreadnought@users.noreply.github.com>
2019-10-15 23:23:08 -0400

    Update docker_logs.sh

--- multinode_integration_tests/docker_logs.sh
+++ multinode_integration_tests/docker_logs.sh
@@ -2,4 +2,4 @@
 
 CONTAINER_NAME="$1"
 docker start "$CONTAINER_NAME"
-docker exec -it "$CONTAINER_NAME" cat /node_root/home/Prometheus-Network_rCURRENT.log
+docker exec -it "$CONTAINER_NAME" cat /node_root/home/PrometheusNode_rCURRENT.log
