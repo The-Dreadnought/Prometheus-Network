17768bc064e47aada0504116995af909b8a792ca
The-Dreadnought <56526236+The-Dreadnought@users.noreply.github.com>
2019-10-16 00:33:47 -0400

    Update graph_connectivity_test.rs

--- multinode_integration_tests/tests/graph_connectivity_test.rs
+++ multinode_integration_tests/tests/graph_connectivity_test.rs
@@ -1,13 +1,14 @@
 // Copyright (c) 2017-2019, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
 
+
 use multinode_integration_tests_lib::multinode_gossip::{
     parse_gossip, GossipType, MultinodeGossip, StandardBuilder,
 };
 use multinode_integration_tests_lib::neighborhood_constructor::construct_neighborhood;
-use multinode_integration_tests_lib::substratum_node::SubstratumNode;
-use multinode_integration_tests_lib::substratum_node_cluster::SubstratumNodeCluster;
-use multinode_integration_tests_lib::substratum_real_node::{
-    NodeStartupConfigBuilder, SubstratumRealNode,
+use multinode_integration_tests_lib::prometheus_node::PrometheusNode;
+use multinode_integration_tests_lib::prometheus_node_cluster::PrometheusNodeCluster;
+use multinode_integration_tests_lib::prometheus_real_node::{
+    NodeStartupConfigBuilder, PrometheusRealNode,
 };
 use node_lib::neighborhood::gossip_acceptor::MAX_DEGREE;
 use node_lib::neighborhood::neighborhood_test_utils::db_from_node;
@@ -19,7 +20,7 @@ use std::time::Duration;
 #[test]
 fn graph_connects_but_does_not_over_connect() {
     let neighborhood_size = 5;
-    let mut cluster = SubstratumNodeCluster::start().unwrap();
+    let mut cluster = PrometheusNodeCluster::start().unwrap();
 
     let first_node = cluster.start_real_node(
         NodeStartupConfigBuilder::standard()
@@ -35,7 +36,7 @@ fn graph_connects_but_does_not_over_connect() {
                     .build(),
             )
         })
-        .collect::<Vec<SubstratumRealNode>>();
+        .collect::<Vec<PrometheusRealNode>>();
     let last_node = real_nodes.last().unwrap();
     let mock_node =
         cluster.start_mock_node_with_public_key(vec![10000], &PublicKey::new(&[1, 2, 3, 4]));
@@ -50,7 +51,7 @@ fn graph_connects_but_does_not_over_connect() {
         _ => panic!("Received unexpected Gossip when expecting Introduction"),
     }
     let standard_gossip = StandardBuilder::new()
-        .add_substratum_node(&mock_node, 100)
+        .add_prometheus_node(&mock_node, 100)
         .half_neighbors(mock_node.public_key(), last_node.public_key())
         .build();
     mock_node
@@ -89,7 +90,7 @@ fn lots_of_stalled_nodes_dont_prevent_acceptance_of_new_node() {
     }
     let full_neighbor_key = &db.add_node(make_node_record(2345, true)).unwrap();
     db.add_arbitrary_full_neighbor(root_node.public_key(), full_neighbor_key);
-    let mut cluster = SubstratumNodeCluster::start().unwrap();
+    let mut cluster = PrometheusNodeCluster::start().unwrap();
     let (_, root_node, _) = construct_neighborhood(&mut cluster, db, vec![]);
     let new_node =
         cluster.start_mock_node_with_public_key(vec![5050], &PublicKey::new(&[3, 4, 5, 6]));
