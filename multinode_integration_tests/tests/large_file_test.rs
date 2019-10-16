1a65eedc5305af8e575db308f9fa308d1072bf78
The-Dreadnought <56526236+The-Dreadnought@users.noreply.github.com>
2019-10-16 00:35:10 -0400

    Update large_file_test.rs

--- multinode_integration_tests/tests/large_file_test.rs
+++ multinode_integration_tests/tests/large_file_test.rs
@@ -1,10 +1,10 @@
 // Copyright (c) 2017-2019, Substratum LLC (https://substratum.net) and/or its affiliates. All rights reserved.
 
 use multinode_integration_tests_lib::rest_utils::RestServer;
-use multinode_integration_tests_lib::substratum_node::SubstratumNode;
-use multinode_integration_tests_lib::substratum_node_cluster::SubstratumNodeCluster;
-use multinode_integration_tests_lib::substratum_real_node::{
-    make_consuming_wallet_info, NodeStartupConfigBuilder, SubstratumRealNode,
+use multinode_integration_tests_lib::prometheus_node::PrometheusNode;
+use multinode_integration_tests_lib::prometheus_node_cluster::PrometheusNodeCluster;
+use multinode_integration_tests_lib::prometheus_real_node::{
+    make_consuming_wallet_info, NodeStartupConfigBuilder, PrometheusRealNode,
 };
 use std::thread;
 use std::time::Duration;
@@ -14,7 +14,7 @@ const REQUEST_BYTES: u64 = 157_286_400;
 
 #[test]
 fn downloading_a_file_larger_than_available_memory_doesnt_kill_node_but_makes_it_stronger() {
-    let mut cluster = SubstratumNodeCluster::start().expect("starting cluster");
+    let mut cluster = PrometheusNodeCluster::start().expect("starting cluster");
     let originating_node = cluster.start_real_node(
         NodeStartupConfigBuilder::standard()
             .memory(MAXIMUM_KBYTES)
@@ -31,7 +31,7 @@ fn downloading_a_file_larger_than_available_memory_doesnt_kill_node_but_makes_it
                     .build(),
             )
         })
-        .collect::<Vec<SubstratumRealNode>>();
+        .collect::<Vec<PrometheusRealNode>>();
 
     let rest_server = RestServer { name: "ptolemy" };
     rest_server.start();
