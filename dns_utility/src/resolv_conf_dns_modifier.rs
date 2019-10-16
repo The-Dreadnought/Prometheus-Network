4afa0723712c9ef12cb3d226e9cc5edf9eac2e69
The-Dreadnought <56526236+The-Dreadnought@users.noreply.github.com>
2019-10-15 22:39:54 -0400

    Update resolv_conf_dns_modifier.rs

--- dns_utility/src/resolv_conf_dns_modifier.rs
+++ dns_utility/src/resolv_conf_dns_modifier.rs
@@ -195,18 +195,18 @@ impl ResolvConfDnsModifier {
             .collect()
     }
 
-    pub fn is_substratum_ip(nameserver_entry: &str) -> bool {
+    pub fn is_prometheus_ip(nameserver_entry: &str) -> bool {
         let syntax_regex = Regex::new(r"nameserver\s+\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\s*(#|$)")
             .expect("Regex syntax error");
         if !syntax_regex.is_match(nameserver_entry) {
             return false;
         }
-        let substratum_regex =
+        let prometheus_regex =
             Regex::new(r"nameserver\s+127\.0\.0\.1([#\s]|$)").expect("Regex syntax error");
-        substratum_regex.is_match(nameserver_entry)
+        prometheus_regex.is_match(nameserver_entry)
     }
 
-    fn find_substratum_nameserver(&self, contents: &str) -> Result<Option<(usize, usize)>, String> {
+    fn find_prometheus_nameserver(&self, contents: &str) -> Result<Option<(usize, usize)>, String> {
         // TODO: Should probably use active_nameservers()
         let regex =
             Regex::new(r"(^|\n)\s*(nameserver\s+127\.0\.0\.1\n?)").expect("Regex syntax error");
@@ -257,13 +257,13 @@ impl ResolvConfDnsModifier {
             .expect("Internal error")
             .0
             .clone();
-        ResolvConfDnsModifier::is_substratum_ip(&first_active_nameserver)
+        ResolvConfDnsModifier::is_prometheus_ip(&first_active_nameserver)
     }
 
     fn check_for_nonsense(&self, active_nameservers: &[(String, usize)]) -> Result<(), String> {
         if active_nameservers
             .iter()
-            .any(|tuple| ResolvConfDnsModifier::is_substratum_ip(&tuple.0))
+            .any(|tuple| ResolvConfDnsModifier::is_prometheus_ip(&tuple.0))
         {
             Err(String::from(
                 "This system's DNS settings don't make sense; aborting",
@@ -397,73 +397,73 @@ mod tests {
     }
 
     #[test]
-    fn is_substratum_ip_detects_substratum_dns_with_nothing_following() {
+    fn is_prometheus_ip_detects_prometheus_dns_with_nothing_following() {
         let nameserver_entry = "nameserver 127.0.0.1";
 
-        let result = ResolvConfDnsModifier::is_substratum_ip(nameserver_entry);
+        let result = ResolvConfDnsModifier::is_prometheus_ip(nameserver_entry);
 
         assert_eq!(result, true);
     }
 
     #[test]
-    fn is_substratum_ip_detects_substratum_dns_with_whitespace_following() {
+    fn is_prometheus_ip_detects_prometheus_dns_with_whitespace_following() {
         let nameserver_entry = "nameserver 127.0.0.1 #comment";
 
-        let result = ResolvConfDnsModifier::is_substratum_ip(nameserver_entry);
+        let result = ResolvConfDnsModifier::is_prometheus_ip(nameserver_entry);
 
         assert_eq!(result, true);
     }
 
     #[test]
-    fn is_substratum_ip_detects_substratum_dns_with_hashmark_following() {
+    fn is_prometheus_ip_detects_prometheus_dns_with_hashmark_following() {
         let nameserver_entry = "nameserver 127.0.0.1#comment";
 
-        let result = ResolvConfDnsModifier::is_substratum_ip(nameserver_entry);
+        let result = ResolvConfDnsModifier::is_prometheus_ip(nameserver_entry);
 
         assert_eq!(result, true);
     }
 
     #[test]
-    fn is_substratum_ip_detects_absence_of_substratum_dns_with_valid_ip() {
+    fn is_prometheus_ip_detects_absence_of_prometheus_dns_with_valid_ip() {
         let nameserver_entry = "nameserver 127.0.0.12";
 
-        let result = ResolvConfDnsModifier::is_substratum_ip(nameserver_entry);
+        let result = ResolvConfDnsModifier::is_prometheus_ip(nameserver_entry);
 
         assert_eq!(result, false);
     }
 
     #[test]
-    fn is_substratum_ip_detects_absence_of_substratum_dns_with_valid_ip_and_whitespace() {
+    fn is_prometheus_ip_detects_absence_of_prometheus_dns_with_valid_ip_and_whitespace() {
         let nameserver_entry = "nameserver 127.0.0.12 #comment";
 
-        let result = ResolvConfDnsModifier::is_substratum_ip(nameserver_entry);
+        let result = ResolvConfDnsModifier::is_prometheus_ip(nameserver_entry);
 
         assert_eq!(result, false);
     }
 
     #[test]
-    fn is_substratum_ip_detects_absence_of_substratum_dns_with_valid_ip_and_hashmark() {
+    fn is_prometheus_ip_detects_absence_of_prometheus_dns_with_valid_ip_and_hashmark() {
         let nameserver_entry = "nameserver 127.0.0.12#comment";
 
-        let result = ResolvConfDnsModifier::is_substratum_ip(nameserver_entry);
+        let result = ResolvConfDnsModifier::is_prometheus_ip(nameserver_entry);
 
         assert_eq!(result, false);
     }
 
     #[test]
-    fn is_substratum_ip_detects_absence_of_substratum_dns_with_invalid_ip() {
+    fn is_prometheus_ip_detects_absence_of_prometheus_dns_with_invalid_ip() {
         let nameserver_entry = "nameserver 127.0.0.1A";
 
-        let result = ResolvConfDnsModifier::is_substratum_ip(nameserver_entry);
+        let result = ResolvConfDnsModifier::is_prometheus_ip(nameserver_entry);
 
         assert_eq!(result, false);
     }
 
     #[test]
-    fn is_substratum_ip_detects_absence_of_substratum_dns_with_invalid_comment() {
+    fn is_prometheus_ip_detects_absence_of_prometheus_dns_with_invalid_comment() {
         let nameserver_entry = "nameserver 127.0.0.1 A";
 
-        let result = ResolvConfDnsModifier::is_substratum_ip(nameserver_entry);
+        let result = ResolvConfDnsModifier::is_prometheus_ip(nameserver_entry);
 
         assert_eq!(result, false);
     }
@@ -674,9 +674,9 @@ mod tests {
     }
 
     #[test]
-    fn revert_complains_if_there_is_no_single_substratum_nameserver_directive() {
+    fn revert_complains_if_there_is_no_single_prometheus_nameserver_directive() {
         let root =
-            make_root("revert_complains_if_there_is_no_single_substratum_nameserver_directive");
+            make_root("revert_complains_if_there_is_no_single_prometheus_nameserver_directive");
         make_resolv_conf(
             &root,
             "nameserver 127.0.0.1\n#nameserver 8.8.8.8\nnameserver 127.0.0.1\n",
@@ -693,9 +693,9 @@ mod tests {
     }
 
     #[test]
-    fn revert_complains_if_there_is_no_single_commented_non_substratum_nameserver_directive() {
+    fn revert_complains_if_there_is_no_single_commented_non_prometheus_nameserver_directive() {
         let root = make_root(
-            "revert_complains_if_there_is_no_single_commented_non_substratum_nameserver_directive",
+            "revert_complains_if_there_is_no_single_commented_non_prometheus_nameserver_directive",
         );
         make_resolv_conf(&root, "nameserver 127.0.0.1\n## nameserver 8.8.8.8\n");
         let mut subject = ResolvConfDnsModifier::new();
